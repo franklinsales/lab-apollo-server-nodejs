c0651b4dc16ea8562352f2c19e7adcc18b75c071
Franklin Sales <franklin.salesx@gmail.com>
2025-05-13 16:41:26 -0300

    feat: define the book schema and the operations (Query and Mutation)

---
+++ src/schema/book.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-server';
+
+// Schema Definition (SDL - Schema Definition Language)
+// Here we define the structure of our data and the operations we can perform on it.
+// We are using the gql template literal tag to define our schema in a more readable way.
+export default gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+
+  type Mutation {
+    addBook(title: String!, author: String!): Book
+  }
+`;
+
+// The concept here is to define a schema that describes the data structure (Book) and the operations (Query and Mutation) that can be performed on that data.
